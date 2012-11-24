94709445b5124ff005436d31abe034495f1f0d4f
Mohamad Mesbah <mm49307@gmail.com>
2012-11-24 16:07:16 +0330

    I have Corrected the little mistake in mehrdad codes in Main.py. I have explained this in google group! Thanks Bye

--- Main.py
+++ Main.py
@@ -8,7 +8,6 @@ import sql
 
 if __name__ == '__main__':
     queries = []
-    queryDic = {}
     lines = []
     flag = 0
     if len(sys.argv) == 2:
@@ -35,6 +34,7 @@ if __name__ == '__main__':
             lines.append(line)
             line = sys.stdin.readline()
     for i in range(0, len(lines) / 2):
+		queryDic = {}
         queryDic['sql'] = lines[i * 2]
         queryDic['time'] = int(lines[i * 2 + 1])
         queries.append(queryDic)
